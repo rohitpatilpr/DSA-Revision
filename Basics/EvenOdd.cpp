a6232bb96e12ac3f2575fdc727d91ba5f6ce824d
Rohit Patil <67288441+rohitpatilpr@users.noreply.github.com>
2025-02-23 01:47:31 +0530

    Create EvenOdd.cpp

---
+++ Basics/EvenOdd.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int no;
+	
+	cout<<"Enter a Number: "<<endl;
+	cin>>no;
+	
+	if(no % 2 == 0)
+	{
+	    cout<<no<<" is even";
+	}
+	else
+	{
+	    cout<<no<<" is odd";
+	}
+
+}
